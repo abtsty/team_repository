7183292e5ccc8601e3eb5f725cc129f3ce1a577b
oopshelga <134417737+oopshelga@users.noreply.github.com>
2023-08-19 11:50:32 +0300

    task4

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -9,8 +9,36 @@
 // ЗАДАЧА 2
 // ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
 
-const priceOne = Number(prompt("Введіть ціну телевізора"));
-const priceTwo = +prompt("Введіть ціну холодильника");
-const priceThree = +prompt("Введіть ціну ноутбука");
+//const priceOne = Number(prompt("Введіть ціну телевізора"));
+//const priceTwo = +prompt("Введіть ціну холодильника");
+//const priceThree = +prompt("Введіть ціну ноутбука");
 
-console.log(priceOne + priceTwo + priceThree);
+//console.log(priceOne + priceTwo + priceThree);
+
+
+////ЗАДАЧА 3
+////ВИКОРИСТОВУЮЧИ КОНСТРУКЦІЮ IF...ELSE , НАПИШИ КОД, ЯКИЙ БУДЕ ЗАПИТУВАТИ: 
+////"СКІЛЬКИ МІСЯЦІВ МАЄ РІК", ЯКЩО КОРИСТУВАЧ ВВОДИТЬ 12 ТО У КОНСОЛІ ПИШЕ ВІРНО, ЯКЩО НЕ ПРАВИЛЬНО КОРИСТУВАЧ ВВІВ, ТОДІ У КОНСОЛІ ПИШЕ 
+////"НЕ ЗНАЄТЕ? 12"
+
+// const month = Number(prompt("How monthes in the year?"));
+// if (month === 12) {
+//     console.log("correct");
+// } else {
+//   console.log("incorrect")
+// }
+ 
+// ЗАДАЧА 4
+// НАПИШІТЬ ПРОГРАМУ, ЯКА ДОЗВОЛЯТИМЕ ЮЗЕРУ ВВОДИТИ ЧИСЛО І ОПРИДІЛЯТИМЕ ЧИ ЦЕ ПАРНЕ ЧИСЛО , ЧИ НЕ ПАРНЕ.
+
+//const number = Number(prompt("Парне?"));
+//1 V
+// if (number % 2 === 0) {
+//     console.log(true);
+    
+// } else {
+//     console.log(false);
+//}
+//2 V
+// const newNumber = number % 2 === 0 ? true : false;
+// console.log(newNumber)
