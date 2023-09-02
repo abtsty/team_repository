a76b5f0fab36dfbab35703ab9ad2a97bf033205a
AnnaStylee <136710517+AnnaStylee@users.noreply.github.com>
2023-09-02 12:08:12 +0300

    tzd3

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -23,3 +23,32 @@
 // for (let key of userKeys) {
 //     console.log(`${key}: ${user[key]}`);
 // }
+
+// ЗАДАЧА 3
+// Створи обєкт calculator(калькулятор) з 3-ма методами:
+// read()- запитує 2 значення у юзера і зберігає їх як значення обєкта;
+// sum() -повертає суму збережених значень; 
+// mult() -перемножує значення збережені і повертає результат;
+
+ const calculator = {
+ a: null,
+ b: null,
+
+     read() {
+         this.a = prompt("valueA")
+         this.b = prompt("valueB")
+  },
+
+     sum() {
+         return Number(this.a) + Number(this.b);
+  },
+
+     mult() {
+         return this.a * this.b;
+  },
+};
+
+
+calculator.read();
+console.log(`sum`, calculator.sum());
+console.log(`mult`, calculator.mult());
