63a19d39ea064a0969e7fc3f0cc7014b098b3c76
abtsty <andrey.barylov@gmail.com>
2023-09-02 11:18:52 +0300

    new

---
+++ js/lesson-2.js
@@ -0,0 +1,25 @@
+// ЗАДАЧА 1
+// Напишіть скрипт,для обєкта user в послідовності:
+
+//  добавляє поле mood із значенням 'happy'
+// замінює значення hobby на 'skydiving'
+// замінює значення  premium на false
+// виводить отримане обєкта user в форматі ключ:значення використовуючи  Object.keys() и for...of
+
+// const user = {
+//     name: "Mango",
+//     age: 20,
+//     hobby: "html",
+//     premium: true,
+// };
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// const userKeys = Object.keys(user);
+// // console.log(userKeys);
+
+// for (let key of userKeys) {
+//     console.log(`${key}: ${user[key]}`);
+// }
