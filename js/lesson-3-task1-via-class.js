f519ac90ed7a62f666c6f3bfa758fce92ebb95f4
BestStepInUa <beststep.in.ua@gmail.com>
2023-09-16 11:30:52 +0300

    Lesson-3-task1-via-class

---
+++ js/lesson-3-task1-via-class.js
@@ -0,0 +1,31 @@
+class Person {
+    constructor({ firstName, lastName, age, gender, interest }) {
+        this.firstName = firstName,
+        this.lastName = lastName,
+        this.age = age,
+        this.gender = gender,
+        this.interest = interest
+    }
+    
+    bio() {
+        console.log(`Привіт я ${this.firstName} мені ${this.age} років. Мені подобається ${this.interest}`);
+    }
+
+    greeting = function () {
+        console.log(`Привіт, я ${this.firstName}`);
+    }
+}
+
+const newPerson = new Person(
+    {
+        firstName: "Mango",
+        lastName: "Pango",
+        age: 25,
+        gender: "male",
+        interest: "HTML"
+    }
+);
+
+console.log(newPerson);
+newPerson.bio();
+newPerson.greeting();
--- js/lesson-3.js
+++ js/lesson-3.js
@@ -14,32 +14,32 @@
 //   },
 // };
 
-function Person(firstName, lastName, age, gender, interest) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.gender = gender;
-    this.interest = interest;
+// function Person(firstName, lastName, age, gender, interest) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.gender = gender;
+//     this.interest = interest;
 
-    // this.bio = function () {
-    //     console.log(
-    //         `Привіт ${this.firstName} мені ${this.age} років. Мені подобається ${this.interest}`
-    //     );
-    // };
-    // this.greeting = function () {
-    //     console.log(`Привіт, я ${this.firstName}`);
-    // };
-}
-Person.prototype.bio = function () {
-    console.log(
-        `Привіт ${this.firstName} мені ${this.age} років. Мені подобається ${this.interest}`
-    );
-};
-Person.prototype.greeting = function () {
-    console.log(`Привіт, я ${this.firstName}`);
-};
-const newPerson = new Person("Mango", "Cooper", 19, "female", "JS");
+//     // this.bio = function () {
+//     //     console.log(
+//     //         `Привіт ${this.firstName} мені ${this.age} років. Мені подобається ${this.interest}`
+//     //     );
+//     // };
+//     // this.greeting = function () {
+//     //     console.log(`Привіт, я ${this.firstName}`);
+//     // };
+// }
+// Person.prototype.bio = function () {
+//     console.log(
+//         `Привіт ${this.firstName} мені ${this.age} років. Мені подобається ${this.interest}`
+//     );
+// };
+// Person.prototype.greeting = function () {
+//     console.log(`Привіт, я ${this.firstName}`);
+// };
+// const newPerson = new Person("Mango", "Cooper", 19, "female", "JS");
 
-console.log(newPerson);
-newPerson.bio();
-newPerson.greeting();
+// console.log(newPerson);
+// newPerson.bio();
+// newPerson.greeting();
